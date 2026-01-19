cd03b7eef35e0e8edb835b8193ff16a368a934c5
Kimboyun00 <bykim1827@gmail.com>
2026-01-19 14:01:00 +0900

    Update next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  devIndicators: {
-    buildActivity: false,
-    buildActivityPosition: "bottom-right",
-  },
 };
 
 export default nextConfig;
